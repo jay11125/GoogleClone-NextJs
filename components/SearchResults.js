351c8556fdb6c4ca1f6bcf6a6322c290d845f03d
Jay Agrawal <jayagrawal8090@gmail.com>
2022-07-03 18:55:57 +0530

    created results section

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -1,3 +1,5 @@
+import Parser from "html-react-parser";
+
 export default function SearchResults({ results }) {
   return (
     <div className="w-full mx-auto px-3 sm:pl-[5%] md:pl-[14%] lg:pl-52">
@@ -5,6 +7,19 @@ export default function SearchResults({ results }) {
         About {results.searchInformation.formattedTotalResults} results (
         {results.searchInformation.formattedSearchTime} seconds)
       </p>
+      {results.items.map((result) => (
+        <div key={result.link} className="max-w-xl mb-8">
+          <div className="group">
+            <a href={result.link} className="text-sm truncate">
+              {result.formattedUrl}
+            </a>
+            <a href={result.link} className="group-hover:underline decoration-blue-800">
+              <h2 className="truncate text-xl font-medium text-blue-800">{result.title}</h2>
+            </a>
+          </div>
+          <p className="text-gray-600">{Parser(result.htmlSnippet)}</p>
+        </div>
+      ))}
     </div>
   );
 }
